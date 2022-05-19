b098d501c4a771c86cea3a244096e81527a1989c
amchu <austin.chu77@gmail.com>
2022-05-19 15:59:26 -0700

    second commit

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import UE_door_script from '../../assets/UE_door_scrip_pic.png';
 import fastapipic from '../../assets/fastapi-socialmedia.png';
+import html_website from '../../assets/html-website.png';
 import ReactPlayer from 'react-player';
+
 
 import './projects.css';
 const Projects = () => (
@@ -21,6 +23,20 @@ const Projects = () => (
         <div className='post-preview'>
           <h6 className='post-title'>VRCHAT World</h6>
           <p className='post-intro'>Created and Designed a VRChat world using Unity, Blender and VRChat SDK. Created custom stuctures and really enjoyed messing around with building and VRChat's SDK.</p>
+        </div>
+      </div>
+      <div className='post'>
+        <img className='thumbnail' src={html_website} />
+        <div className='post-preview'>
+          <h6 className='post-title' ><a href='https://www.public.asu.edu/~amchu1/' target='_blank'>HTML Website (click me)</a></h6>
+          <p className='post-intro'>First personal project. Created a personal website using HTML and CSS. Utilized Javascript to save changes to theme colors the user picks. Also has a form that allows users to send emails to me.</p>
+        </div>
+      </div>
+      <div className='post'>
+        <ReactPlayer width='100%' height='180px' url='https://youtu.be/qVv6pGCJNbg' />
+        <div className='post-preview'>
+          <h6 className='post-title'>Pediatric Healthcare Management App</h6>
+          <p className='post-intro'>Collaborated in a team of 5 members and utilized JavaFX and SQLite. Encouraged to use Agile Method for development and hosted Sprint meetings bi-weekly to ensure we met project deadlines.</p>
         </div>
       </div>
       <div className='post'>
